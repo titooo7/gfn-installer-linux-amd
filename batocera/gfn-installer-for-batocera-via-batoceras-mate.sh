3ea7c7ed21282d5df6673f6e0a42335108affe89
titooo7 <juanmartinbravo@gmail.com>
2025-08-27 21:40:44 +0000

    Create gfn-installer-for-batocera-via-batoceras-mate.sh

---
+++ batocera/gfn-installer-for-batocera-via-batoceras-mate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Download the latest version of the installation script
+echo "Downloading the latest installer script..."
+curl -L -o "install-gfn-batocera.sh" "https://raw.githubusercontent.com/titooo7/gfn-installer-linux-amd/refs/heads/main/batocera/install-gfn-batocera.sh"
+
+# Make the downloaded script executable
+chmod +x "/userdata/system/install-gfn-batocera.sh
+
+
+# Define the path to conty
+conty=/userdata/system/pro/steam/conty.sh
+
+# The script you want to execute inside the container
+# IMPORTANT: Use the full path as seen from Batocera
+SCRIPT_TO_RUN="/userdata/system/install-gfn-batocera.sh"
+
+# Execute the script inside the container using MATE's fish shell
+"$conty" \
+        --bind /userdata/system/containers/storage /var/lib/containers/storage \
+        --bind /userdata/system/flatpak /var/lib/flatpak \
+        --bind /userdata/system/etc/passwd /etc/passwd \
+        --bind /userdata/system/etc/group /etc/group \
+        --bind /var/run/nvidia /run/nvidia \
+        --bind /userdata/system /home/batocera \
+        --bind /sys/fs/cgroup /sys/fs/cgroup \
+        --bind /userdata/system /home/root \
+        --bind /etc/fonts /etc/fonts \
+        --bind /userdata /userdata \
+        --bind /newroot /newroot \
+        --bind / /batocera \
+fish -c "$SCRIPT_TO_RUN"
