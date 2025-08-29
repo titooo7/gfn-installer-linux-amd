9e62faca62b0efcd50ba361dc047e945604b7efe
titooo7 <juanmartinbravo@gmail.com>
2025-08-29 21:31:34 +0000

    Update install-gfn.sh

--- arch/install-gfn.sh
+++ arch/install-gfn.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # GPU check: Verify that the graphics card is from AMD
-if ! lspci | grep -i 'VGA compatible controller' | grep -iq 'AMD'; then
-    echo -e "\e[1;31mERROR: AMD GPU not detected.\e[0m"
-    echo -e "\e[0;33mThis installer is specifically designed for systems with AMD graphics cards.\e[0m"
-    echo "Script will now exit."
+if ! lspci | grep -i 'VGA' | grep -qi 'amd\|advanced micro devices'; then
+    echo "This script requires an AMD GPU. Exiting."
     exit 1
 fi
 echo ""
