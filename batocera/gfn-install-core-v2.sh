eedfc51f805abf0f6d9853be07799950effa38d7
titooo7 <juanmartinbravo@gmail.com>
2025-09-01 15:54:54 +0000

    Update gfn-install-core-v2.sh

--- batocera/gfn-install-core-v2.sh
+++ batocera/gfn-install-core-v2.sh
@@ -270,7 +270,7 @@ for dest in "${!files[@]}"; do
     fi
 done
 echo "If you installed Amazon Luna or Xcloud we just made a symlinks of those apps from Ports to Cloud Gaming."
-ln -s /userdata/roms/ports/xcloud.sh /userdata/roms/ports/AmazonLuna.sh /userdata/roms/geforcenow/
+ln -s /userdata/roms/ports/xcloud.sh /userdata/roms/ports/AmazonLuna.sh /userdata/roms/geforcenow/ || true
 echo ""
 echo "✅ Go to settings / User Interfaces and select the theme 'ES-THEME-CARBON-GFN'."
 echo ""
